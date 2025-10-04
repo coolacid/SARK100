0c6b9b5e14129fe52c7be9836a9cd4755f9af338
Jason Kendall <jakendall@gmail.com>
2025-10-04 15:01:01 -0400

    Return a customer iter instead of a yield

--- example.py
+++ example.py
@@ -3,8 +3,12 @@ from pysark100 import sark100
 
 s = sark100()
 
-data = list(s.scan_band("40m", buffer_pct=0.10, step=10000, progress=True))
-# data = list(s.scan_band("hf", buffer_pct=0, step=10000, progress=True))
-print(s.data.get_data())
-s.data.plot(show_bands=True)
-s.data.plot_interactive()
+data = s.scan_band("40m", buffer_pct=0.10, step=10000, progress=True)
+# data = s.scan_band("hf", buffer_pct=0, step=10000, progress=True)
+
+#for x in data:
+#    print(x)
+
+print(data.get_dataframe())
+data.plot(show_bands=True)
+data.plot_interactive()
--- pysark100/__init__.py
+++ pysark100/__init__.py
@@ -5,10 +5,81 @@ from tqdm import tqdm
 from pysark100.collector import Sark100Collector
 from pysark100.bands import bands, generate_band_frequencies
 
+class sark100Scan:
+    data_values = ['swr', 'r', 'x', 'z']
+
+    def __init__(self, parent, start, end, step=1000, progress=True):
+        self.device = parent.device
+        self.data = Sark100Collector()
+        self.step = step
+        self.start = start
+        self.cur_freq = start
+        self.end = end + step
+        self.progress = progress
+        if self.progress:
+            total = math.ceil(((end - start) / step))
+            self.pbar = tqdm(total=total)
+
+        self.device.write(f"scan {start} {end} {step}\r\n".encode())
+
+    def _ensure_full(self):
+        while self.cur_freq < self.end:
+            try:
+                self.__next__()
+            except StopIteration:
+                break
+
+    def get_dataframe(self):
+        self._ensure_full()
+        return self.data.df
+
+    def plot(self, *args, **kwargs):
+        self._ensure_full()
+        self.data.plot(*args, **kwargs)
+
+    def plot_interactive(self, *args, **kwargs):
+        self._ensure_full()
+        self.data.plot_interactive(*args, **kwargs)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            data = self.device.readline().decode('utf-8').strip()
+
+            # If we're run over just stop the Iteration
+            if self.cur_freq > self.end:
+                raise StopIteration
+
+            # We skip responses we don't care about
+            if data in ["Start", "", ">>"]:
+                continue
+
+            # The SARK100 has said it's ended it's data
+            if data == "End":
+                self.cur_freq += self.step
+                if self.progress:
+                    self.pbar.close()
+                raise StopIteration
+
+            # The SARK100 has said there's been an error, so raise an error
+            if "Error" in data:
+                print(data)
+                raise StopIteration
+
+            # We've got data, break out of our loop
+            break
+
+        if self.progress:
+            self.pbar.update(1)
+            self.pbar.set_postfix({"Freqency": self.cur_freq})
+
+        self.data.add_measurement(self.cur_freq, data)
+        self.cur_freq += self.step
+        return dict(zip(self.data_values, data.split(",")))
 
 class sark100:
-    data_vaules = ['swr', 'r', 'x', 'z']
-
     def __init__(self, port='/dev/ttyUSB0'):
         self.device = serial.Serial(
                 port=port,  # Replace with your port name (e.g., 'COM1' on Windows)
@@ -17,43 +88,15 @@ class sark100:
                 timeout=5,
                 stopbits=serial.STOPBITS_ONE
         )
-        self.data = Sark100Collector()
 
     def scan(self, start, end, step = 1000, progress=False):
-        freq = start
-        end += step
         total = math.ceil(((end - start) / step))
         print(f"Getting data between {start} and {end} with a step of {step} for a total of {total} data points.")
-        self.device.write(f"scan {start} {end} {step}\r\n".encode())
-        if progress:
-            pbar = tqdm(total=total)
-        while True:
-            data = self.device.readline().decode('utf-8').strip()
-            if data in ["Start", "", ">>"]:
-                continue
-            if data == "End":
-                break
-            if "Error" in data:
-                print(data)
-                break
-            scan_result = dict(zip(self.data_vaules, data.split(",")))
-            self.data.add_measurement(freq, data)
-            freq += step
-            if freq > end:
-                break
-            if progress:
-                pbar.update(1)
-                pbar.set_postfix({"Freqency": freq})
-            yield {freq: scan_result}
-        if progress:
-            pbar.close()
+        return sark100Scan(self, start, end, step)
 
     def scan_band(self, band, buffer_pct=0.15, step=1000, progress=False):
         freq_list = generate_band_frequencies(band, buffer_pct=buffer_pct, step_hz=step)
         return self.scan(freq_list[0], freq_list[-1], step, progress=progress)
-
-    def plot(self):
-        pass
 
     def __end__(self):
         self.device.close()
